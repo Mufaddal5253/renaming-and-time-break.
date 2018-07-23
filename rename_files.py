f2232d0c30d9789f275690bdb0c6350d02271874
Mufaddal5253 <mariahatimbd@gmail.com>
2018-07-23 13:50:30 +0500

    Here are two programs

--- rename_files.py
+++ rename_files.py
@@ -1,11 +1,11 @@
 import os
 def rename_files():
     # 1. file name from folder
-    file_list = os.listdir(r"D:\Mufaddal2153\prank pics")
+    file_list = os.listdir(r"prank pics")
     #print(file_list)
     saved_path = os.getcwd()
     print("Current Working Directory is "+saved_path)
-    os.chdir(r"D:\Mufaddal2153\prank pics")
+    os.chdir(r"prank pics")
     # 2. for each file rename file name
     for file_name in file_list:
         os.rename(file_name, file_name.strip('0123456789'))
